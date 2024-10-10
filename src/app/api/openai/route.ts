92782bbcdf0612e7e3cf510d505070d4d986b49c
Prajhub <pzlmagar@gmail.com>
2024-10-10 11:48:00 +0545

    Fixed QOL

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     const { prompt } = await req.json();
 
