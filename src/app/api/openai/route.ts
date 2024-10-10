a81936fe3c575effca1c6816fb2499348628e445
Prajhub <pzlmagar@gmail.com>
2024-10-10 12:05:39 +0545

    Added saftey settings to the API

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -1,9 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+];
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  safetySettings: safetySettings,
+});
 
 export async function POST(req: NextRequest) {
   try {
@@ -21,8 +47,6 @@ export async function POST(req: NextRequest) {
     const result = await model.generateContent(finalPrompt);
 
     return NextResponse.json({ message: result.response.text() });
-
-    //return new StreamingTextResponse(stream);
   } catch (error) {
     console.error("Error processing request:", error);
     return NextResponse.json("Can't process the request right now.");
